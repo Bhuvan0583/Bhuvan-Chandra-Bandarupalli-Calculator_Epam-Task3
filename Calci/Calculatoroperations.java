a95f0757922e769d1bc5db6a65daa1214cea7d63
Bhuvan0583 <60144959+Bhuvan0583@users.noreply.github.com>
2020-02-11 00:42:51 +0530

    Add files via upload

---
+++ Calci/Calculatoroperations.java
@@ -0,0 +1,26 @@
+package Calci;
+
+public class Calculatoroperations {
+			public int addition(int a,int b)
+			{
+				return a+b;
+			}
+			public int substraction(int a,int b)
+			{
+				return a-b;
+			}
+			public int multiply(int a,int b)
+			{
+				return a*b;
+			}
+			public float divide(int a,int b)
+			{
+				float c=(float)a/b;
+				return c;
+			}
+			public int modulus(int a,int b)
+			{
+				return a%b;
+			}
+	}
+
